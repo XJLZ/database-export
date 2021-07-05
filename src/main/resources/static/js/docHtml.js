69365523326039548741675ba0e2201f13476377
XJL <xxxx@xxxx.com>
2021-07-05 14:38:13 +0800

    nothing

--- src/main/resources/static/js/docHtml.js
+++ src/main/resources/static/js/docHtml.js
@@ -1,70 +1,72 @@
 $(function () {
-    loadInfo();
+  loadInfo();
 });
+
 function loadInfo() {
-    $.ajax({
-        url: ctx+"v2/getTableData",
-        data: {
-            ip :ip,
-            port :port,
-            dbName :dbName,
-            userName :userName,
-            password :password,
-            dbKind: dbKind
-        },
-        success:function (data) {
-            let resultCode = data.resultCode;
-            if(resultCode == '000000'){
-                loadTable(data.params);
-            }else{
-                alert(data.resultMsg);
-            }
-        }
-    });
+  $.ajax({
+    url: ctx + "v2/getTableData",
+    data: {
+      ip: ip,
+      port: port,
+      dbName: dbName,
+      userName: userName,
+      password: password,
+      dbKind: dbKind
+    },
+    success: function (data) {
+      let resultCode = data.resultCode;
+      if (resultCode == '000000') {
+        loadTable(data.params);
+      } else {
+        alert(data.resultMsg);
+      }
+    }
+  });
 }
+
 function loadTable(params) {
-    let headerList = params.headerList;
-    let fieldList = params.fieldList;
-    let tableDetailInfo = params.tableDetailInfo;
-    let tableHtml = '';
-    let headerHtml = '';
-    let headDiv = '<div style="width: 300px;height: 100%;float: left;position: relative;font-size: 22px;margin-top: 20px;flex: 1;overflow:auto;overflow-x: auto;"><ul>';
-    let tailDiv = '</ul></div><div style="position: relative; overflow: auto;height: 100%;flex: 1;float: right;width: 1000px;">';
-    let lis = '';
-    for(let i=0;i<headerList.length;i++){
-        headerHtml += '<th>'+headerList[i]+'</th>'
-    }
-    for(let i=0;i<tableDetailInfo.length;i++){
-        let table = tableDetailInfo[i];
-        let tabsColumn = table.tabsColumn;
-        let tbodyHtml = "";
-        for (let j = 0; j < tabsColumn.length; j++) {
-            tbodyHtml += '<tr>';
-            for (let k = 0; k < fieldList.length; k++) {
-                let column = tabsColumn[j];
-                let columnElement = column[fieldList[k]];
-                if(columnElement == undefined || columnElement == '' ){
-                    columnElement = "无";
-                }
-                tbodyHtml += '<td>' + columnElement + '</td>';
-            }
-            tbodyHtml += '</tr>';
+  let headerList = params.headerList;
+  let fieldList = params.fieldList;
+  let tableDetailInfo = params.tableDetailInfo;
+  let tableHtml = '';
+  let headerHtml = '';
+  let headDiv = '<div style="width: 300px;height: 100%;float: left;position: relative;font-size: 22px;margin-top: 20px;flex: 1;overflow:auto;overflow-x: auto;"><ul>';
+  let tailDiv = '</ul></div><div style="position: relative; overflow: auto;height: 100%;flex: 1;float: right;width: 1000px;">';
+  let lis = '';
+  for (let i = 0; i < headerList.length; i++) {
+    headerHtml += '<th>' + headerList[i] + '</th>'
+  }
+  for (let i = 0; i < tableDetailInfo.length; i++) {
+    let table = tableDetailInfo[i];
+    let tabsColumn = table.tabsColumn;
+    let tbodyHtml = "";
+    for (let j = 0; j < tabsColumn.length; j++) {
+      tbodyHtml += '<tr>';
+      for (let k = 0; k < fieldList.length; k++) {
+        let column = tabsColumn[j];
+        let columnElement = column[fieldList[k]];
+        if (columnElement == undefined || columnElement == '') {
+          columnElement = "无";
         }
-
-        lis += '<li><a href="#'+table.tableName+'">'+table.tableName+'</a></li>'
-
-        tableHtml +=
-            '<table id="'+table.tableName+'" class="table table-bordered table-hover">'+
-            // '<caption style="font-weight: bolder;font-size: 30px">'+table.tableName+'('+table.tableComments+')'+'</caption>'+
-            '<caption style="font-weight: bolder;font-size: 30px;text-align: center;">'+table.tableComments+'('+table.tableName+')'+'</caption>'+
-            '<thead>'+
-            '<tr>'+headerHtml+'</tr>'+
-            '</thead>'+
-            '<tbody>'+tbodyHtml+'</tbody>'+
-            '</table>';
+        tbodyHtml += '<td>' + columnElement + '</td>';
+      }
+      tbodyHtml += '</tr>';
     }
-    const htmls = headDiv + lis + tailDiv + tableHtml + '</div>';
-    console.log(htmls);
-    $("#body").empty();
-    $("#body").append(htmls);
+
+    lis += '<li><a href="#' + table.tableName + '">' + table.tableName + '</a></li>'
+
+    tableHtml +=
+      '<table id="' + table.tableName + '" class="table table-bordered table-hover">' +
+      // '<caption style="font-weight: bolder;font-size: 30px">'+table.tableName+'('+table.tableComments+')'+'</caption>'+
+      '<caption style="font-weight: bolder;font-size: 30px;text-align: center;">' + table.tableComments + '(' + table.tableName + ')' + '</caption>' +
+      '<thead>' +
+      '<tr>' + headerHtml + '</tr>' +
+      '</thead>' +
+      '<tbody>' + tbodyHtml + '</tbody>' +
+      '</table>';
+  }
+  const htmls = headDiv + lis + tailDiv + tableHtml + '</div>';
+  // console.log(htmls);
+  $("#body").empty();
+  $("#body").append(htmls);
 }
--- src/test/java/com/pomzwj/DatabaseExportApplicationTests.java
+++ src/test/java/com/pomzwj/DatabaseExportApplicationTests.java
@@ -1,15 +1,70 @@
 package com.pomzwj;
 
 
+import com.deepoove.poi.XWPFTemplate;
+import com.deepoove.poi.data.*;
+import com.pomzwj.constant.TemplateFileConstants;
+import com.pomzwj.domain.SegmentData;
+import com.pomzwj.domain.TableData;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @SpringBootTest
 public class DatabaseExportApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() throws Exception {
+        List<TableData> subData = new ArrayList<>();
+        List<Map<String, Object>> subDataMap = new ArrayList<>();
+        for (int j = 0; j < 10; j++) {
+            Map<String, Object> table = new HashMap<>(1);
+            List<RowRenderData> rows = new ArrayList<>();
+            //获取表头
+            List<String> headerList = new ArrayList<>();
+            headerList.add("姓名");
+            headerList.add("年龄");
+            headerList.add("性别");
+            headerList.add("住址");
+            headerList.add("电话");
+            RowRenderData raw0 = Rows.of(headerList.toArray(new String[headerList.size()])).textColor("FFFFFF").bgColor("4471c4").center().create();
+            rows.add(raw0);
+            for (int i = 0; i < 10; i++) {
+                List<String> dataBody = new ArrayList<>();
+                dataBody.add("张三" + i + " " + j);
+                dataBody.add("21" + i);
+                dataBody.add("男" + i);
+                dataBody.add("中国北京" + i);
+                dataBody.add("11231_" + i);
+                rows.add(Rows.of(dataBody.toArray(new String[dataBody.size()])).center().create());
+            }
+//            TableData segmentData = new TableData();
+//            segmentData.setTable(Tables.create(rows.toArray(new RowRenderData[rows.size()])));
+//            subData.add(segmentData);
+
+            Map<String, Object> tableMap = new HashMap<>(1);
+            tableMap.put("table",Tables.create(rows.toArray(new RowRenderData[rows.size()])));
+            subDataMap.add(tableMap);
+        }
+
+        Map<String, Object> table = new HashMap<>(1);
+//        table.put("tables", Includes.ofLocal("D:\\test\\sub_model.docx").setRenderModel(subData).create());
+        table.put("tables", Includes.ofLocal("D:\\test\\sub_model.docx").setRenderModel(subDataMap).create());
+
+//        XWPFTemplate template = XWPFTemplate.compile("D:\\test\\import.docx").render(
+//                new HashMap<String, Object>(){{
+//                    put("table",  Tables.create(rows.toArray(new RowRenderData[rows.size()])));
+//                }});
+        XWPFTemplate template = XWPFTemplate.compile(new File("D:\\test\\import.docx")).render(table);
+        template.writeAndClose(new FileOutputStream("D:\\test\\output.docx"));
+    }
 
 }
